5d77430c21fcd4b62e87c382d2b7de60e2f7f767
inoplanetyanya <inoplanetyanya97@gmail.com>
2025-03-11 22:45:54 +0300

    mobx, node types, build rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: [
-            {
-                find: '@',
-                replacement: path.resolve(__dirname, 'src'),
-            },
-        ],
-        extensions: ['.tsx', '.ts', '.scss']
-    }
-})
+	plugins: [react()],
+	resolve: {
+		alias: [
+			{
+				find: "@",
+				replacement: path.resolve(__dirname, "src"),
+			},
+		],
+		extensions: [".tsx", ".ts", ".scss"],
+	},
+	build: {
+		rollupOptions: {
+			external: ["use-sync-external-store/shim"],
+		},
+	},
+});
